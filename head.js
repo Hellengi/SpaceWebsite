b3102ec68e7b2cafe240d090cac036d15e344201
OikawaD <uranus.oj@gmail.com>
2023-12-08 20:42:56 +0300

    1-2

--- head.js
+++ head.js
@@ -1,6 +1,7 @@
 document.write(`
     <meta charset="utf-8" />
     <meta name="keywords" lang="ru" content="Кометы Астероиды" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Астероиды и кометы</title>
     <link rel="icon" type="image/png" href="Comet.png" />
     <link rel="stylesheet" href="style.css" />
