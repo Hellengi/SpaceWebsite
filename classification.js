6fa8702cd0aa7568ac7b411d422f937a3e80f08a
OikawaD <uranus.oj@gmail.com>
2023-12-14 00:30:24 +0300

    1.4.1

--- classification.js
+++ classification.js
@@ -1,19 +1,27 @@
-let CurrentActive = 1
 const SmallBlock = document.getElementsByClassName("small-block")
 const SmallBlockBorder = document.getElementById("small-block-border")
 const ImageContainer = document.getElementById("image-container")
 SmallBlock[0].addEventListener("click", function () {
-    CurrentActive = 1
     SmallBlockBorder.style.transform = "translateX(0)"
     ImageContainer.style.transform = "translateX(0)"
 })
 SmallBlock[1].addEventListener("click", function () {
-    CurrentActive = 2
-    SmallBlockBorder.style.transform = "translateX(9vw)"
-    ImageContainer.style.transform = "translateX(-60vw)"
+    if(window.innerWidth / window.innerHeight > 1) {
+        SmallBlockBorder.style.transform = "translateX(9vw)"
+        ImageContainer.style.transform = "translateX(-60vw)"
+    }
+    else {
+        SmallBlockBorder.style.transform = "translateX(21vw)"
+        ImageContainer.style.transform = "translateX(-90vw)"
+    }
 })
 SmallBlock[2].addEventListener("click", function () {
-    CurrentActive = 3
-    SmallBlockBorder.style.transform = "translateX(18vw)"
-    ImageContainer.style.transform = "translateX(-120vw)"
+    if(window.innerWidth / window.innerHeight > 1) {
+        SmallBlockBorder.style.transform = "translateX(18vw)"
+        ImageContainer.style.transform = "translateX(-120vw)"
+    }
+    else {
+        SmallBlockBorder.style.transform = "translateX(42vw)"
+        ImageContainer.style.transform = "translateX(-180vw)"
+    }
 })
