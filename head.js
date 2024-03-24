3138be1d0b03d43a91ea9cd0142214e39ad4cc9b
OikawaD <uranus.oj@gmail.com>
2024-03-24 12:47:59 +0300

    1.5.1

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ document.write(`
     <meta charset="utf-8" />
     <meta name="keywords" lang="ru" content="Кометы Астероиды" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">   
     <title>Астероиды и кометы</title>
     <link rel="icon" type="image/png" href="Comet.png" />
     <link rel="stylesheet" href="style.css" />
